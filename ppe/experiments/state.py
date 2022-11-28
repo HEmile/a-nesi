140e8e97d051058f703854a860510e6e3633f7b9
Emile <e.van.krieken@vu.nl>
2022-11-28 15:58:40 +0100

    Attempt for general symbolic pruner

--- ppe/experiments/state.py
+++ ppe/experiments/state.py
@@ -132,6 +132,7 @@ class MNISTAddState(StateBase):
 
     def symbolic_pruner(self) -> torch.Tensor:
         if len(self.y) < self.N + 1:
+            # Label. Rarely filters anything
             if len(self.y) == 0:
                 return torch.ones((2,)).unsqueeze(0)
             onez = torch.ones((10,))
@@ -144,26 +145,45 @@ class MNISTAddState(StateBase):
                 onez_without_nine = onez_without_nine.to(self.device)
                 onez = onez.to(self.device)
                 # TODO: Test if this is ever nonzero
-                is_19s = self.y[0] * torch.prod(torch.stack([self.oh_state[i + 1][:, 9] for i in range(self.N - 1)], -1), -1)
+                is_9s = 1
+                if self.N - 1 > 0:
+                    is_9s = torch.prod(torch.stack([self.oh_state[i + 1][:, 9] for i in range(self.N - 1)], -1), -1)
+                is_19s = self.y[0] * is_9s
                 is_19s = is_19s.unsqueeze(-1)
-                return is_19s * onez_without_nine + (1 - is_19s).unsqueeze(-1) * onez
+                return is_19s * onez_without_nine + (1 - is_19s) * onez
             return onez
-        if self.N == 1:
-            # We'll do other cases later
-            ny = self.query_to_number()
-            if len(self.w) == 0:
-                rang = torch.arange(10, device=ny.device).unsqueeze(0)
-                ny = ny.unsqueeze(-1)
-                first_comp = rang <= ny
-                second_comp = rang >= ny - 9
-                return torch.logical_and(first_comp, second_comp).int()
-            else:
-                if len(self.w[0].shape) > len(ny.shape):
-                    ny = ny.unsqueeze(-1)
-                d2 = ny - self.w[0]
-                # TODO: This assumes for every d1 there is a solution (ie 0 <= constraint - d1 <= 9)
-                return torch.nn.functional.one_hot(d2, 10).int()
-        raise NotImplementedError()
+        kth_digit = len(self.w)
+        if kth_digit < self.N:
+            # First number
+            nw = 0
+            if kth_digit > 0:
+                sum_w = torch.stack([10 ** (kth_digit - i) * self.w[i] for i in range(kth_digit)], -1).sum(-1)
+                nw = sum_w.unsqueeze(-1)
+            sum_y = torch.stack([10 ** (kth_digit - i + 1) * self.y[i] for i in range(kth_digit + 2)], -1).sum(-1)
+            rang = torch.arange(10, device=sum_y.device).unsqueeze(0)
+            ny = sum_y.unsqueeze(-1)
+            first_comp = nw + rang <= ny
+
+            # Note: This part is pretty confusing lol. Need to really explain this well in the paper
+            # On the last digit we generate, we can at most have 10**N-1 - 1 to add to it to make up y
+            #  Before that, the next digits can together sum to above 10**kth_digit to allow one step further.
+            #  There is one more edge case. Consider y = 109. Taking the first 0, we can only have 9 to add to it,
+            #  meaning the second digit has to be maximum (99), and 9 + 99 = only 108!
+            #  So if the digit of y coming after the one considered is 9, _and_ y is even, we have to remove an option
+            second_comp = nw + rang >= ny - (10 ** (kth_digit + 1) -
+                                             (1 if kth_digit == self.N - 1 else (self.y[kth_digit + 2] == 9).unsqueeze(-1).int()))
+            keep = torch.logical_and(first_comp, second_comp).int()
+            return keep
+
+        # Second number. Deterministic
+        ny = self.query_to_number()
+        n1 = torch.stack([10 ** (self.N - i - 1) * self.w[i] for i in range(self.N)], -1).sum(-1)
+        if len(n1.shape) > len(ny.shape):
+            ny = ny.unsqueeze(-1)
+        n2 = ny - n1
+        d = torch.floor(n2 / (10 ** (2 * self.N - kth_digit - 1)) % 10).long()
+        # TODO: This assumes for every d1 there is a solution (ie 0 <= constraint - d1 <= 9)
+        return torch.nn.functional.one_hot(d, 10).int()
 
     def finished_generating_y(self) -> bool:
         return len(self.y) == self.N + 1
